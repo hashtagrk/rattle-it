d43c6229bf63b436b2a6c9d78c48097b8c62306a
skyflow-ghuser1 <github.user1@skyflow.com>
2023-10-24 19:05:53 +0530

    fi;les

--- TF-EKS_assesment1/terraform-rattle/vpc/main.tf
+++ TF-EKS_assesment1/terraform-rattle/vpc/main.tf
@@ -1,16 +1,18 @@
-provider "aws" {
-  # The AWS region in which all resources will be created
-  region = var.aws_region
+# provider "aws" {
+#   # The AWS region in which all resources will be created
+#   region = var.aws_region
 
-  # Provider version 2.X series is the latest, but has breaking changes with 1.X series.
-  version = "~> 4.29.0"
+#   # Provider version 2.X series is the latest, but has breaking changes with 1.X series.
+#   version = "~> 4.29.0"
 
-  # Only these AWS Account IDs may be operated on by this template
-  allowed_account_ids = [var.aws_account_id]
-}
+#   # Only these AWS Account IDs may be operated on by this template
+#   allowed_account_ids = [var.aws_account_id]
+# }
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -32,8 +34,20 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -50,4 +64,15 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_eip" "nat" {
   count = length(aws_subnet.public_subnet)
+}
+resource "aws_route" "route_to_igw" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_igw.id
+}
+
+resource "aws_route" "route_to_igw" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_igw.id
 }
